82d3b0ca9aceb41be0c20593c5fd3dc7c9ce67ed
Sahil Garg <sahil.garg78272026@gmail.com>
2023-04-30 21:32:51 +0530

    Update 03_string_functions.py

--- 03_string_functions.py
+++ 03_string_functions.py
@@ -2,6 +2,18 @@ story ="once upon a time , there was a programmer named Sahil Garg"
 s1 = 'amaama RhokHo MY nAME'
 
 #String Functions
+'''
+['__add__', '__class__', '__contains__', '__delattr__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', 
+'__getitem__', '__getnewargs__', '__gt__', '__hash__', '__init__', '__init_subclass__', '__iter__', '__le__', '__len__', '__lt__', 
+'__mod__', '__mul__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__rmod__', '__rmul__', '__setattr__', 
+'__sizeof__', '__str__', '__subclasshook__', 
+'capitalize', 'casefold', 'center', 'count', 'encode', 'endswith', 'expandtabs', 'find', 'format', 'format_map', 'index', 
+'isalnum', 'isalpha', 'isascii', 'isdecimal', 'isdigit', 'isidentifier', 'islower', 'isnumeric', 'isprintable', 'isspace', 
+'istitle', 'isupper', 'join', 'ljust', 'lower', 'lstrip', 'maketrans', 'partition', 'removeprefix', 'removesuffix', 'replace', 
+'rfind', 'rindex', 'rjust', 'rpartition', 'rsplit', 'rstrip', 'split', 'splitlines', 'startswith', 'strip', 'swapcase', 'title', 
+'translate', 'upper', 'zfill']
+'''
+
 
 print(len(story)) # returns the number of character in string
 print(story.endswith("Garg")) # Return true if the string ends with Mentioned word
@@ -17,3 +29,8 @@ print(s1.center(55,'$')) # will add '$' and keep the original string in center a
 print(s1.count('Rho',1,20)) # return the number of times a particular value appeared in the string. # 1 and 20 are start and end points which are optional
 print(s1.encode('utf-8',errors='ignore')) # encode the string in 
 
+
+
+
+
+##
